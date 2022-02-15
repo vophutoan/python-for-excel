2070adb94b359094674365c6045819d709902af2
Võ Phú Toàn <43806778+vophutoan@users.noreply.github.com>
2022-02-15 20:33:41 +0700

    Add files via upload

---
+++ 002. CÁCH XỬ LÝ DỮ LIỆU CỦA 1 CỘT.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[16]:
+
+
+import pandas as pd 
+
+file_mau = pd.read_excel (r'C:\Users\Vo Phu Toan\Contacts\Desktop\New folder\danhsach1.xls') 
+
+#file_mau.X1 #Lấy giá trị của cột X1, X1 không có khoảng cách
+#file_mau['X2'] #Lấy giá trị của cột X2, X2 có khoảng cách
+
+#file_mau.X1.sum() #Lấy Tổng của cột X1
+
+#file_mau.X1.mean() #Trung bình cộng cột X1
+
+#file_mau.X1.nunique() #Lấy số lượng của cột X1
+
+#file_mau['X1'].value_counts() #Đếm số lượng các thành phần có trong cột X1
+
+
+# In[ ]:
+
+
+
+
