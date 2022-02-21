47e5be62f2e268c983c6fe94755ee2b1e93879e7
Võ Phú Toàn <43806778+vophutoan@users.noreply.github.com>
2022-02-21 14:34:11 +0700

    Add files via upload

---
+++ 003. CÁCH XỬ LÝ DỮ LIỆU NHIỀU CỘT (MULTIPLE COLUMNS).py
@@ -0,0 +1,18 @@
+import pandas as pd 
+
+file_mau = pd.read_excel (r'C:\Users\Vo Phu Toan\Contacts\Desktop\New folder\danhsach1.xls') 
+
+
+#tong_hop = ['Giá','Tổng'] #LẤY DỮ LIỆU TỪ NHIỀU CỘT
+#file_mau[tong_hop].sum()
+
+
+#file_mau[tong_hop].describe() #LẤY DỮ LIỆU THỐNG KÊ
+
+
+#file_mau['Tổng'] = '10000' #THAY ĐỔI THÔNG SỐ CẢ 1 CỘT
+#file_mau.head()
+
+
+#file_mau['USD'] = file_mau['Tổng']/25000 #THÊM MỚI 1 CỘT
+#file_mau.head()
