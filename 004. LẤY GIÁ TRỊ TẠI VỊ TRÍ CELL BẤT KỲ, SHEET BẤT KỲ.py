f8f7ec87e01e9d555b6de1892ad1b5f4ea3a9ac8
Võ Phú Toàn <43806778+vophutoan@users.noreply.github.com>
2022-02-23 18:39:43 +0700

    Add files via upload

---
+++ 004. LẤY GIÁ TRỊ TẠI VỊ TRÍ CELL BẤT KỲ, SHEET BẤT KỲ.py
@@ -0,0 +1,14 @@
+import pandas as pd 
+
+file_mau = pd.read_excel (r'C:\Users\Vo Phu Toan\Contacts\Desktop\New folder\danhsach1.xls') 
+
+
+#file_mau.loc[:,:] #Lấy giá trị tại vị trí nào đó, lọc theo tên cột
+
+#file_mau.iloc[:,:] #Lấy giá trị tại vị trí nào đó, lọc theo số thứ tự cột
+
+#file_mau.head()
+
+#LẤY DỮ LIỆU THEO SHEET
+#file_mau = pd.read_excel (r'C:\Users\Vo Phu Toan\Contacts\Desktop\New folder\danhsach1.xls', sheet_name ='')
+
